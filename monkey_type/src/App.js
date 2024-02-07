6e713ce5f33993db725ab5ecf69df36d36cc221f
ROtem <rotemsaadi@gmail.com>
2024-02-07 16:49:33 +0200

    basic words generate

--- monkey_type/src/App.js
+++ monkey_type/src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useRef } from "react";
+import { generate } from "random-words";
+
+const WORDS_NUMBER = 400;
+const SECONDS = 30;
 
 function App() {
+  const [words, setWords] = useState([]);
+
+  useEffect(() => {
+    setWords(generateWords());
+  }, []);
+
+  function generateWords() {
+    return new Array(WORDS_NUMBER).fill(null).map(() => generate());
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="section">
+        <div className="is-size-1 has-text-centered has-text-primary">
+          <h2>{SECONDS}</h2>
+        </div>
+      </div>
+      <div className="control is-expanded section">
+        <input className="input" />
+      </div>
+      <div className="section">
+        <button className="button is-info is-fullwidth">Start</button>
+      </div>
+
+      <div className="section">
+        {words.map((word, i) => (
+          <span key={i}>
+            <span>{word}</span>
+            <span> </span>
+          </span>
+        ))}
+      </div>
     </div>
   );
 }
